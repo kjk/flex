dd86f6ddcc79a01d030e53515ff497d9fcbcd5f9
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2017-07-22 19:32:19 -0700

    port compute padding tests

---
+++ compute_padding_test.go
@@ -0,0 +1,22 @@
+package flex
+
+import "testing"
+
+func TestComputed_layout_padding(t *testing.T) {
+	root := YGNodeNew()
+	YGNodeStyleSetWidth(root, 100)
+	YGNodeStyleSetHeight(root, 100)
+	YGNodeStyleSetPaddingPercent(root, YGEdgeStart, 10)
+
+	YGNodeCalculateLayout(root, 100, 100, YGDirectionLTR)
+
+	assertFloatEqual(t, 10, YGNodeLayoutGetPadding(root, YGEdgeLeft))
+	assertFloatEqual(t, 0, YGNodeLayoutGetPadding(root, YGEdgeRight))
+
+	YGNodeCalculateLayout(root, 100, 100, YGDirectionRTL)
+
+	assertFloatEqual(t, 0, YGNodeLayoutGetPadding(root, YGEdgeLeft))
+	assertFloatEqual(t, 10, YGNodeLayoutGetPadding(root, YGEdgeRight))
+
+	YGNodeFreeRecursive(root)
+}
