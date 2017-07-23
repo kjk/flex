3a3a396ab1b0a2aba9162eee11fec11b0fcb2a44
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2017-07-22 19:31:29 -0700

    port compute margin tests

---
+++ compute_margin_test.go
@@ -0,0 +1,22 @@
+package flex
+
+import "testing"
+
+func TestComputed_layout_margin(t *testing.T) {
+	root := YGNodeNew()
+	YGNodeStyleSetWidth(root, 100)
+	YGNodeStyleSetHeight(root, 100)
+	YGNodeStyleSetMarginPercent(root, YGEdgeStart, 10)
+
+	YGNodeCalculateLayout(root, 100, 100, YGDirectionLTR)
+
+	assertFloatEqual(t, 10, YGNodeLayoutGetMargin(root, YGEdgeLeft))
+	assertFloatEqual(t, 0, YGNodeLayoutGetMargin(root, YGEdgeRight))
+
+	YGNodeCalculateLayout(root, 100, 100, YGDirectionRTL)
+
+	assertFloatEqual(t, 0, YGNodeLayoutGetMargin(root, YGEdgeLeft))
+	assertFloatEqual(t, 10, YGNodeLayoutGetMargin(root, YGEdgeRight))
+
+	YGNodeFreeRecursive(root)
+}
