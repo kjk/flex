5dd6159d8c1734a6685891131815139ff1b58c86
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2017-07-22 19:43:03 -0700

    ported dimension tests

---
+++ dimension_test.go
@@ -0,0 +1,92 @@
+package flex
+
+import "testing"
+
+func TestWrap_child(t *testing.T) {
+	config := YGConfigNew()
+
+	root := YGNodeNewWithConfig(config)
+
+	root_child0 := YGNodeNewWithConfig(config)
+	YGNodeStyleSetWidth(root_child0, 100)
+	YGNodeStyleSetHeight(root_child0, 100)
+	YGNodeInsertChild(root, root_child0, 0)
+	YGNodeCalculateLayout(root, YGUndefined, YGUndefined, YGDirectionLTR)
+
+	assertFloatEqual(t, 0, YGNodeLayoutGetLeft(root))
+	assertFloatEqual(t, 0, YGNodeLayoutGetTop(root))
+	assertFloatEqual(t, 100, YGNodeLayoutGetWidth(root))
+	assertFloatEqual(t, 100, YGNodeLayoutGetHeight(root))
+
+	assertFloatEqual(t, 0, YGNodeLayoutGetLeft(root_child0))
+	assertFloatEqual(t, 0, YGNodeLayoutGetTop(root_child0))
+	assertFloatEqual(t, 100, YGNodeLayoutGetWidth(root_child0))
+	assertFloatEqual(t, 100, YGNodeLayoutGetHeight(root_child0))
+
+	YGNodeCalculateLayout(root, YGUndefined, YGUndefined, YGDirectionRTL)
+
+	assertFloatEqual(t, 0, YGNodeLayoutGetLeft(root))
+	assertFloatEqual(t, 0, YGNodeLayoutGetTop(root))
+	assertFloatEqual(t, 100, YGNodeLayoutGetWidth(root))
+	assertFloatEqual(t, 100, YGNodeLayoutGetHeight(root))
+
+	assertFloatEqual(t, 0, YGNodeLayoutGetLeft(root_child0))
+	assertFloatEqual(t, 0, YGNodeLayoutGetTop(root_child0))
+	assertFloatEqual(t, 100, YGNodeLayoutGetWidth(root_child0))
+	assertFloatEqual(t, 100, YGNodeLayoutGetHeight(root_child0))
+
+	YGNodeFreeRecursive(root)
+
+	YGConfigFree(config)
+}
+
+func TestWrap_grandchild(t *testing.T) {
+	config := YGConfigNew()
+
+	root := YGNodeNewWithConfig(config)
+
+	root_child0 := YGNodeNewWithConfig(config)
+	YGNodeInsertChild(root, root_child0, 0)
+
+	root_child0_child0 := YGNodeNewWithConfig(config)
+	YGNodeStyleSetWidth(root_child0_child0, 100)
+	YGNodeStyleSetHeight(root_child0_child0, 100)
+	YGNodeInsertChild(root_child0, root_child0_child0, 0)
+	YGNodeCalculateLayout(root, YGUndefined, YGUndefined, YGDirectionLTR)
+
+	assertFloatEqual(t, 0, YGNodeLayoutGetLeft(root))
+	assertFloatEqual(t, 0, YGNodeLayoutGetTop(root))
+	assertFloatEqual(t, 100, YGNodeLayoutGetWidth(root))
+	assertFloatEqual(t, 100, YGNodeLayoutGetHeight(root))
+
+	assertFloatEqual(t, 0, YGNodeLayoutGetLeft(root_child0))
+	assertFloatEqual(t, 0, YGNodeLayoutGetTop(root_child0))
+	assertFloatEqual(t, 100, YGNodeLayoutGetWidth(root_child0))
+	assertFloatEqual(t, 100, YGNodeLayoutGetHeight(root_child0))
+
+	assertFloatEqual(t, 0, YGNodeLayoutGetLeft(root_child0_child0))
+	assertFloatEqual(t, 0, YGNodeLayoutGetTop(root_child0_child0))
+	assertFloatEqual(t, 100, YGNodeLayoutGetWidth(root_child0_child0))
+	assertFloatEqual(t, 100, YGNodeLayoutGetHeight(root_child0_child0))
+
+	YGNodeCalculateLayout(root, YGUndefined, YGUndefined, YGDirectionRTL)
+
+	assertFloatEqual(t, 0, YGNodeLayoutGetLeft(root))
+	assertFloatEqual(t, 0, YGNodeLayoutGetTop(root))
+	assertFloatEqual(t, 100, YGNodeLayoutGetWidth(root))
+	assertFloatEqual(t, 100, YGNodeLayoutGetHeight(root))
+
+	assertFloatEqual(t, 0, YGNodeLayoutGetLeft(root_child0))
+	assertFloatEqual(t, 0, YGNodeLayoutGetTop(root_child0))
+	assertFloatEqual(t, 100, YGNodeLayoutGetWidth(root_child0))
+	assertFloatEqual(t, 100, YGNodeLayoutGetHeight(root_child0))
+
+	assertFloatEqual(t, 0, YGNodeLayoutGetLeft(root_child0_child0))
+	assertFloatEqual(t, 0, YGNodeLayoutGetTop(root_child0_child0))
+	assertFloatEqual(t, 100, YGNodeLayoutGetWidth(root_child0_child0))
+	assertFloatEqual(t, 100, YGNodeLayoutGetHeight(root_child0_child0))
+
+	YGNodeFreeRecursive(root)
+
+	YGConfigFree(config)
+}
